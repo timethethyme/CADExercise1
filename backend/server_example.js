17bdabef569d004eaa331e445730c0357a56b0e5
timethethyme <35400358+timethethyme@users.noreply.github.com>
2020-04-29 12:53:04 +0200

    Addded example views, added example server js

---
+++ backend/server_example.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const ejs = require('ejs')
+const path = require('path')
+
+const app = express();
+
+app.set('view engine', 'ejs')
+//Set path for frontend views
+const view_path = path.join(__dirname, '/..', '/frontend')
+app.set('views', view_path)
+app.use(express.static(view_path))
+
+app.get('/', (req, res) => res.render('index'))
+app.get('/upload', (req, res) => res.render('upload'))
+
+const port = 3000;
+app.listen(port, () => console.log('Running'))
