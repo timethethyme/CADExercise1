e7fd5d21758fba6c44f05158eb789e8434ea6d17
timethethyme <35400358+timethethyme@users.noreply.github.com>
2020-04-29 13:39:37 +0200

    Added view, example photo and added images route

--- backend/server_example.js
+++ backend/server_example.js
@@ -5,10 +5,15 @@ const path = require('path')
 const app = express();
 
 app.set('view engine', 'ejs')
+
 //Set path for frontend views
 const view_path = path.join(__dirname, '/..', '/frontend')
 app.set('views', view_path)
 app.use(express.static(view_path))
+
+const image_path = path.join(__dirname, '/..', '/assets')
+app.use('/images',  express.static(image_path))
+
 
 app.get('/', (req, res) => res.render('index'))
 app.get('/upload', (req, res) => res.render('upload'))
