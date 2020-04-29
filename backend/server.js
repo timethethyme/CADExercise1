6ba38ccf9935539c7bad8315c59465d297d245a9
LukeCaruzo <schmidt.lucas@googlemail.com>
2020-04-29 11:12:39 +0200

    workflow

--- backend/server.js
+++ backend/server.js
@@ -3,21 +3,23 @@ const assert = require('assert');
 
 const url = 'mongodb://localhost:27017';
 
-const dbName = 'cad';
-
 const client = new MongoClient(url);
+
+const dbName = 'cad';
 
 var db;
 var collection;
 
 client.connect(function(err) {
     assert.equal(null, err);
+
     console.log("Connected successfully to server");
 
     db = client.db(dbName);
     collection = db.collection('documents');
 });
 
+/*
 function insert(data) {
     collection.insertOne(data, function(err, res) {
         if (err) throw err;
@@ -34,15 +36,15 @@ function show() {
     });
 }
 
-/*
 var http = require('http');
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello World\n');
 }).listen(1337, "127.0.0.1");
 console.log('Server running at http://127.0.0.1:1337/');
-*/
+
 
 var myobj = { name: "Test" };
 
 insert(myobj);
+ */
