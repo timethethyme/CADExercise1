837956bf02afa0a3b7ee13e7bb4aac844a943011
timethethyme <35400358+timethethyme@users.noreply.github.com>
2020-05-14 18:06:08 +0200

    Added Bucket and Dynamo connection

--- backend/server.js
+++ backend/server.js
@@ -1,38 +1,3 @@
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
-
-const url = 'mongodb://localhost:27017';
-
-const client = new MongoClient(url);
-
-const dbName = 'cad';
-
-var db;
-var collection;
-
-client.connect(function(err) {
-    assert.equal(null, err);
-
-    console.log("Connected successfully to mongo server!");
-
-    db = client.db(dbName);
-    collection = db.collection('documents');
-});
-
-function insert(data) {
-    collection.insertOne(data, function(err, res) {
-        if (err) throw err;
-        console.log("1 document inserted");
-    });
-}
-
-function show() {
-    collection.find({}).toArray(function(err, docs) {
-        assert.equal(err, null);
-        console.log("Found the following records");
-        console.log(docs)
-    });
-}
 
 const express = require('express');
 const ejs = require('ejs')
@@ -42,105 +7,68 @@ const app = express();
 
 app.set('view engine', 'ejs')
 
-//Set path for frontend views
-const view_path = path.join(__dirname, '/..', '/frontend')
-app.set('views', view_path)
-app.use(express.static(view_path))
-
-const image_path = path.join(__dirname, '/..', '/assets')
-app.use('/images',  express.static(image_path))
-
+const aws = require('aws-sdk')
 const multer = require('multer');
-let upload = multer({dest: image_path})
+const multerS3 = require('multer-s3')
+
+aws.config.update({
+    secretAccessKey: 'XXX',
+    accessKeyId: 'XXX',
+    region: 'eu-central-1'
+});
+
+const s3 = new aws.S3({ /* ... */})
+const ddb = new aws.DynamoDB({apiVersion: '2012-10-17'});
+const docClient = new aws.DynamoDB.DocumentClient();
+const uuid = require('uuid-random');
+
+
+let upload = multer({
+    storage: multerS3({
+        s3: s3,
+        bucket: '404bucket',
+        metadata: function (req, file, cb) {
+            cb(null, {fieldName: file.fieldname});
+        },
+        key: function (req, file, cb) {
+            cb(null, 'public/uploads/' + file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+        },
+        limits: {fileSize: 1000000},
+    })
+})
 
 app.get('/', (req, res) => {
-    collection.find({}).toArray(function(err, docs) {
-        console.log("Docs send to view");
-        console.log(docs)
-        res.render('index', {uploads: docs})
+    const params = {
+        TableName: '404table',
+        ProjectionExpression: "PICTURE_NAME, PICTURE_PATH"
+    }
+
+    docClient.scan(params, function (err, data) {
+        if (err) {
+            console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
+        } else {
+            res.render('index', {uploads: data.Items})
+        }
     });
 })
 app.get('/upload', (req, res) => res.render('upload'))
 app.post('/upload', upload.single('uploaded_file'), function (req, res) {
-    insert({'name': req.body.name,'path': req.file.filename})
+    const params = {
+        TableName: '404table',
+        Item: {
+            'PICTURE_ID': {S: uuid()},
+            'PICTURE_NAME': {S: req.body.name},
+            'PICTURE_PATH': {S: req.file.location}
+        }
+    };
+    ddb.putItem(params, function (err, data) {
+        if (err) {
+            console.log("Error", err);
+        } else {
+            console.log("Success", data);
+        }
+    });
     res.render('upload')
 });
-
 const port = 3000;
-app.listen(port, () => console.log('View server is running!'))
-
-/*
-var http = require('http');
-
-http.createServer(function(request, response){
-
-    //The following code will print out the incoming request text
-    request.pipe(response);
-
-}).listen(8080, '127.0.0.1');
-
-console.log('Listening on port 8080...');
-
-http.get('http://127.0.0.1:8080', (resp) => {
-    let data = '';
-
-    // A chunk of data has been recieved.
-    resp.on('data', (chunk) => {
-        data += chunk;
-    });
-
-    // The whole response has been received. Print out the result.
-    resp.on('end', () => {
-        console.log(JSON.parse(data).explanation);
-    });
-
-}).on("error", (err) => {
-    console.log("Error: " + err.message);
-});
-
-const https = require('https')
-
-const getoptions = {
-    hostname: '127.0.0.1',
-    port: 8080,
-    path: '/get',
-    method: 'GET'
-}
-
-const getreq = https.request(getoptions, res => {
-    console.log(`statusCode: ${res.statusCode}`)
-
-    res.on('data', d => {
-        process.stdout.write(d)
-        show()
-    })
-})
-
-getreq.on('error', error => {
-    console.error(error)
-})
-
-getreq.end()
-
-const postoptions = {
-    hostname: '127.0.0.1',
-    port: 8080,
-    path: '/post',
-    method: 'POST'
-}
-
-const postreq = https.request(postoptions, res => {
-    console.log(`statusCode: ${res.statusCode}`)
-
-    res.on('data', d => {
-        process.stdout.write(d)
-        insert()
-    })
-})
-
-postreq.on('error', error => {
-    console.error(error)
-})
-
-postreq.end();
- */
+app.listen(port, () => console.log('View server is running!'));
--- script.sh
+++ script.sh
@@ -1,39 +1,30 @@
 #!/bin/bash
 
 # installing nodejs on ubuntu
-curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+sudo curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 # installing mongodb
-wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+sudo wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 sudo apt-get update
-sudo apt-get install -y mongodb-org
-sudo apt-get install -y npm
+sudo apt install npm
+sudo apt install -y mongodb-org
+
 
 #setting up the repo
 git clone https://github.com/timethethyme/CADExercise1.git
 cd CADExercise1/backend/
-npm install
+sudo npm install
 sudo npm i -g nodemon
-sudo npm i -g ejs
 cd ..
 cd ..
 chmod 777 -R CADExercise1
 cd CADExercise1/backend/
 
-#ngrok for exposing localhost
-sudo npm install --unsafe-perm -g ngrok
-
-#==============================================================
-#=========REPLACE HERE (even though no longer needed)==========
-#==============================================================
-ngrok authtoken XYZ
-
 # for starting the db
-mkdir -p ./data
-mongod --port 27017 --dbpath ./data &
-sleep 6s
-nodemon server.js &
-sleep 6s
-ngrok http -region=eu 3000
+sudo mkdir -p ./data
+sudo mongod --port 27017 --dbpath ./data &
+sudo sleep 6s
+sudo nodemon server.js &
+sudo sleep 6s
